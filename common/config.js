165c80cce0cdcaf4fcc626c017e971ed45757d3d
Reazul Iqbal <admin@reazul.net>
2021-02-04 09:39:21 +0600

    Moving to production

--- common/config.js
+++ common/config.js
@@ -11,9 +11,9 @@ module.exports = {
   WS_PORT: process.env.WS_PORT || 5000,
   JWT_SECRET: process.env.JWT_SECRET,
   CHAIN_NAME: 'hive',
-  APP_ID: 'dublup-io-dev',
-  SIDECHAIN_ID: 'ssc-testnet-reaz',
-  SIDECHAIN_RPC: 'https://packmanager.dtools.dev/rpc', // 'https://api.hive-engine.com/rpc',
+  APP_ID: 'dublup-io',
+  SIDECHAIN_ID: 'ssc-mainnet-hive',
+  SIDECHAIN_RPC: 'https://api.hive-engine.com/rpc',
   NFT_ISSUE_FEE_SYMBOL: 'BEE',
   NFT_SYMBOL: 'DUBLUP',
   CURRENCY: 'PAL',
@@ -27,8 +27,8 @@ module.exports = {
   ORACLE_STAKE_REQUIREMENT: 10000,
   ACCESS_TOKEN_EXPIRATION: '1d', // 1 day
   REFRESH_TOKEN_EXPIRATION: '90d', // 90 days
-  NODES: ['https://api.hive.blog', 'https://api.pharesim.me', 'https://api.hivekings.com', 'https://rpc.ecency.com', 'https://rpc.ausbit.dev'],
-  ADMINS: ['reazuliqbal', 'aggroed', 'clayboyn'],
+  NODES: ['https://api.hive.blog', 'https://api.pharesim.me', 'https://api.deathwing.me', 'https://api.hivekings.com', 'https://rpc.ecency.com', 'https://rpc.ausbit.dev'],
+  ADMINS: ['dublup', 'reazuliqbal', 'aggroed', 'r0nd0n', 'juliakponsford', 'crimsonclad', 'victoriabsb', 'swelker101', 'isaria', 'clayboyn', 'nealmcspadden'],
   CREATION_FEE_ACCOUNT: 'null',
   REPORTING_DURATION: 1, // in days
   CORRECT_REPORTING_REP_REWARD: 1,
